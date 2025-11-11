903af5c8f5b92d513a9fd235b70c0ef6d10a7f97
Matheus Leite <matheusssleite@outlook.com>
2025-11-11 12:00:16 -0300

    feat: Inserindo imagem genérico nas configurações do projeto

--- Receitas.Repositorio/Comandos/ReceitasComandosRepositorio.cs
+++ Receitas.Repositorio/Comandos/ReceitasComandosRepositorio.cs
@@ -31,7 +31,7 @@ namespace Receitas.Repositorio.Comandos
             var client = new Supabase.Client(supabaseUrl!, supabaseKey);
             await client.InitializeAsync();
 
-            string imagemUrl = string.Empty;
+            string imagemUrl = _configuration["NoImageBucket"]!;
 
             if (receita.Imagem != null) {
                 imagemUrl = InserirImagemReceita(receita, client, bucket!);
@@ -53,7 +53,7 @@ namespace Receitas.Repositorio.Comandos
                 IdTipoReceita = r.IdTipoReceita,
                 DataCriacao = r.DataCriacao,
                 Nome = r.Nome,
-                Imagem = r.Imagem ?? "https://images.unsplash.com/photo-1557499305-bd68d0ad468d?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1262"
+                Imagem = r.Imagem
             }).FirstOrDefault()!;
         }
 
